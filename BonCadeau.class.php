1d9f7a4af2f9365053d3495fd43e2e4a0809fd0b
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-01 12:34:58 +0100

    functionname

--- BonCadeau.class.php
+++ BonCadeau.class.php
@@ -27,8 +27,8 @@ public function showAll(){
 public function AfficherBonCadeau(){?>
 <section>
     <p>Montant du bon: <?=$this->getPriceMontant(); ?></p>
-    <p>Beneficiaire: <?= $this->lastnamebeneficiaire?> <?= $this->firstnamebeneficiaire?></p>
-    <p>Donnateur: <?= $this->lastnamedonnateur?> <?= $this->firstnamedonnateur?></p>
+    <p>Beneficiaire: <?= $this->getFullNameBeneficiaire();?></p>
+    <p>Donnateur: <?= $this->getFullNameDonnateur();?></p>
     <p>Date: <?=$this->date ?></p>
     </section>
     <?php
@@ -40,6 +40,19 @@ public function getPriceMontant() {
     } 
 }
 
+public function getFullNameBeneficiaire(){
+    return $this->lastnamebeneficiaire." ".$this->firstnamebeneficiaire;
+    }
+
+    public function getFullNameDonnateur(){
+        return $this->lastnamedonnateur." ".$this->firstnamedonnateur;
+        }
+
+public function setDate(string $date) {
+    if ($date > 2022/05/05) {
+        $this->date = $date;
+    }
+}
 }
 ?> 
 
@@ -48,12 +61,6 @@ public function getPriceMontant() {
 <h1>CONSIGNE</h1>
 
 <p>
-
-Ensuite créez le fichier bon-cadeau.php. 
-Lors de la réception des informations, vous devrez créer un objet bon cadeau 
-à partir d'une classe bon cadeau. Pour cela, créez un fichier BonCadeau.class.php 
-et créez y une classe BonCadeau qui devra respecter les contraites suivantes:
-
 Contenir toutes les propriétés nécessaires pour stocker les informations d'un 
 bon cadeau provenant du formulaire ainsi qu'un numéro de bon cadeau et une date limite d'utilisation
 Dans le constructeur, créer la valeur de la propriété numéro de bon cadeau à 
@@ -63,8 +70,6 @@ ajoutant un an à la date du bon
 Idéalement, les propriétés devront être protected, vous devrez créer des getter 
 et des setter (ou utiliser les méthodes magiques __get et __set)
 Contenir une méthode permettant d'afficher le bon cadeau avec toutes ses informations en HTML
- 
-
 Dans le fichier bon-cadeau.php, récupérez les données envoyées par le formulaire et 
 utilisez les pour créer un nouvel objet BonCadeau. Affichez ensuite le bon cadeau sur la page.
 
