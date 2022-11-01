1ed1387b1480a26b40ce3b2fe16a9d48cc2a1cc8
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-01 13:27:52 +0100

    functiongenafaire

--- BonCadeau.class.php
+++ BonCadeau.class.php
@@ -55,6 +55,12 @@ public function setDate(string $date) {
     }
 }
 }
+
+
+//creer un nouvel objet stocke dans => constructeur pour passe les donnees 
+$boncadeau1 = new BonCadeau(5000, "Monsieur","Louis","MAdame","LOuisa","2022/07/08");
+echo $boncadeau1->AfficherBonCadeau()."<br>";
+
 ?> 
 
 <link rel="stylesheet" href="style/style.css">
@@ -62,16 +68,37 @@ public function setDate(string $date) {
 <h1>CONSIGNE</h1>
 
 <p>
-Contenir toutes les propriétés nécessaires pour stocker les informations d'un 
-bon cadeau provenant du formulaire ainsi qu'un numéro de bon cadeau et une date limite d'utilisation
+Contenir toutes les propriétés provenant du formulaire ainsi qu'un numéro de bon cadeau et une date limite d'utilisation
 Dans le constructeur, créer la valeur de la propriété numéro de bon cadeau à 
 partir de la date du bon sous la forme: mois - annee - 2 chiffre aléatoire 
 Dans le constructeur, créer la valeur de la propriété date d'expiration en 
 ajoutant un an à la date du bon
-Idéalement, les propriétés devront être protected, vous devrez créer des getter 
-et des setter (ou utiliser les méthodes magiques __get et __set)
-Contenir une méthode permettant d'afficher le bon cadeau avec toutes ses informations en HTML
-Dans le fichier bon-cadeau.php, récupérez les données envoyées par le formulaire et 
-utilisez les pour créer un nouvel objet BonCadeau. Affichez ensuite le bon cadeau sur la page.
-
 </p>
+
+<?php
+/*
+ * L'entrée est constituée de champs séparés par un point-virgule,
+ * et le premier champ est un ID à utiliser comme clé.
+ */
+
+$input = <<<'EOF'
+1;PHP;Likes dollar signs
+2;Python;Likes whitespace
+3;Ruby;Likes blocks
+EOF;
+
+function input_parser($input) {
+    foreach (explode("\n", $input) as $line) {
+        $fields = explode(';', $line);
+        $id = array_shift($fields);
+
+        yield $id => $fields;
+    }
+}
+
+foreach (input_parser($input) as $id => $fields) {
+    echo "$id:\n";
+    echo "    $fields[0]\n";
+    echo "    $fields[1]\n";
+}
+?>
